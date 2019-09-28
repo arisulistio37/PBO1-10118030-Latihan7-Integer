99954f2a75d95b0bf189f51fe177c27909ad9c79
arisulistio37 <arisulistio37@gmail.com>
2019-09-28 17:23:28 +0700

    Initial commit

---
+++ src/pbo1/pkg10118030/latihan7/integer/PBO110118030Latihan7Integer.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118030.latihan7.integer;
+
+/**
+ *
+ * @author 
+ * NAMA     : Ari Sulistio
+ * KELAS    : IF-1
+ * NIM      : 10118030
+ */
+public class PBO110118030Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : "+b);
+        System.out.println("short : "+s);
+        System.out.println("int : "+i);
+        System.out.println("long : "+l);
+        
+    }
+    
+}
